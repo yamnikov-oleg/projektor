8fba64aa5480b930ad4f700d7800a5928509bfb8
Oleg Yamnikov <bgeyts667@gmail.com>
2015-12-25 23:21:08 +0500

    Move logging logic into separate file

---
+++ log.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const (
+	Verbose = true
+)
+
+func logf(format string, a ...interface{}) {
+	if Verbose {
+		fmt.Printf(format, a...)
+	}
+}
+
+func errduring(action string, err error, nextmove string, a ...interface{}) {
+	line := action + ":\n"
+	if err != nil {
+		line += err.Error() + "\n"
+	}
+	if nextmove != "" {
+		line += nextmove + "\n"
+	}
+	logf(line, a...)
+}
--- main.go
+++ main.go
@@ -3,32 +3,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/yamnikov-oleg/go-gtk/gtk"
 )
-
-const (
-	Verbose = true
-)
-
-func logf(format string, a ...interface{}) {
-	if Verbose {
-		fmt.Printf(format, a...)
-	}
-}
-
-func errduring(action string, err error, nextmove string, a ...interface{}) {
-	line := action + ":\n"
-	if err != nil {
-		line += err.Error() + "\n"
-	}
-	if nextmove != "" {
-		line += nextmove + "\n"
-	}
-	logf(line, a...)
-}
 
 func main() {
 	gtk.Init(&os.Args)
