e479b0af8db41d4977e05b0fb060e173c44cf37f
Oleg Yamnikov <bgeyts667@gmail.com>
2015-12-27 00:17:25 +0500

    Styling

--- css.go
+++ css.go
@@ -1,7 +1,20 @@
 package main
 
 const CSS_CODE = `
-GtkWindow {
-background-color: #00f;
-}
+  GtkEntry {
+    background-image: none;
+    border: none;
+    box-shadow: none;
+    font-size: 14pt;
+  }
+  GtkTreeView {
+    background-color: transparent;
+    font-size: 8pt;
+  }
+  GtkTreeView:selected {
+    background-color: #59b2ed;
+  }
+  GtkTreeView.cell {
+    padding: 6px;
+  }
 `
