957617c99df6bec3afa0e62999f2168924670b61
Oleg Yamnikov <bgeyts667@gmail.com>
2017-02-03 20:19:09 +0500

    Only make entries for commands in the PATH and executable files' paths

--- cmd_entries.go
+++ cmd_entries.go
@@ -1,8 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var PathEntries = strings.Split(os.Getenv("PATH"), string(os.PathListSeparator))
+
 func SearchCmdEntries(query string) (list LaunchEntriesList) {
-	if query != "" && !IsInHistory(query) {
+	if query != "" && !IsInHistory(query) && IsCommand(query) {
 		return LaunchEntriesList{NewEntryFromCommand(query)}
 	}
 	return nil
+}
+
+func IsCommand(cmd string) bool {
+	return IsPathExecutable(cmd) || IsCommandInPath(cmd)
+}
+
+func IsPathExecutable(path string) bool {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return IsExecutable(stat)
+}
+
+func IsCommandInPath(cmd string) bool {
+	for _, path := range PathEntries {
+		filePath := fmt.Sprintf("%v%v%v", path, string(os.PathSeparator), cmd)
+		if IsPathExecutable(filePath) {
+			return true
+		}
+	}
+	return false
 }
